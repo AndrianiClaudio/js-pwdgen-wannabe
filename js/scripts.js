1b6b3d8a6fb6a03ad56cca75404e25d5b2bb492b
AndrianiClaudio <claudioandriani1994@gmail.com>
2021-11-18 15:11:29 +0100

    completamento esercizio base

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 // costante: "21"
+const secretNumber = 21;
 // Chiedo il nome all'utente e salvo in una variabile*/
+let firstName = prompt('Inserisci il tuo nome');
+console.log(firstName);
 // Chiedo il cognome all'utente e salvo in una variabile*/
+let surname = prompt ('Inserisci il tuo cognome');
+console.log(surname);
 // Chiedo il colore preferito all'utente e salvo in una variabile*/
+let userColor = prompt ('Inserisci il tuo colore preferito');
+console.log(userColor);
 // Scrivo sulla pagina nome+cognome+colorepreferito+21
+console.log(`${firstName}${surname}${userColor}${secretNumber}`);
+document.writeln(`${firstName}${surname}${userColor}${secretNumber}`);
