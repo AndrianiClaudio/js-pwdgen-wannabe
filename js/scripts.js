67e8dafffca2c4dd1cbd43849e1e9822b25939eb
AndrianiClaudio <claudioandriani1994@gmail.com>
2021-11-18 15:00:35 +0100

    descrizione esercizio

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-alert("Link Js corretto");
+// costante: "21"
+// Chiedo il nome all'utente e salvo in una variabile*/
+// Chiedo il cognome all'utente e salvo in una variabile*/
+// Chiedo il colore preferito all'utente e salvo in una variabile*/
+// Scrivo sulla pagina nome+cognome+colorepreferito+21
